661abc130ea950b0e07623ec721e7e97b66cc9ea
GM <meszaros275@gmail.com>
2019-08-27 14:28:49 +0100

    add some canvas magic

--- resources/views/krypton/index.blade.php
+++ resources/views/krypton/index.blade.php
@@ -110,6 +110,11 @@
             min-height: 100vh;
         }
 
+        * {
+            -webkit-box-sizing: border-box;
+            box-sizing: border-box;
+        }
+
         .text-gray {
             color: #999;
         }
@@ -122,6 +127,10 @@
     </style>
 
 </head>
+
+<canvas style="position: absolute;" id="nokey" width="800" height="800">
+    Your Browser Don't Support Canvas, Please Download Chrome ^_^``
+</canvas>
 
 <body class="d-flex flex-column h-100">
     <header>
@@ -223,6 +232,7 @@
             <span class="text-muted">NO IEO, NO ICO, NO PREMINE, NO AIRDROP! Find us on <a href="https://github.com/kryptonchain" target="_blank"><i class="fab fa-github" style="color: #0088CC;"></i></a>.</span>
         </div>
     </footer>
+    <script src="js/canvas.js"></script>
     <script src="//code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="//stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
